9f004f2a3d2fb1deaf8001bcdab7707a26bd6cd3
eric10179 <eric10179@gmail.com>
2022-03-14 01:46:42 +0800

    Week 3-1 onButtonClick>BLUE

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tx;
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tx = findViewById(R.id.textView);
+        bt = findViewById(R.id.button);
+    }
+
+    public void onButtonClick(View view) {
+        tx.setTextColor(Color.BLUE);
     }
 }
