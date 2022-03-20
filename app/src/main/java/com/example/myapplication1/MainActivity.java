b5a1c7cb2aa340d96d7fdf1407889093eada41af
eric10179 <eric10179@gmail.com>
2022-03-21 00:25:28 +0800

    Week 4-2 add constraintlayout.xml Button One、Button Two constraint alignment

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     TextView tx;
 //    Button bt;
@@ -16,28 +16,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.constraintlayout);
 
-        tx = findViewById(R.id.textView);
-        //bt = findViewById(R.id.button);
+//        tx = findViewById(R.id.textView);
+//        bt = findViewById(R.id.button);
 
 //        bt.setOnClickListener(this);
-        tx.setOnClickListener(this);
+//        tx.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.textView:
-                tx.setTextColor(Color.RED);
-                tx.setText("Hello World!");
-                break;
+//    @Override
+//    public void onClick(View view) {
+//        switch(view.getId()) {
+//            case R.id.textView:
+//                tx.setTextColor(Color.RED);
+//                tx.setText("Hello World!");
+//                break;
 //            case R.id.button:
 //                tx.setTextColor(Color.BLUE);
 //                tx.setText("點我、點我");
-        }
-        //tx.setTextColor(Color.BLUE);
-    }
+//        }
+//        //tx.setTextColor(Color.BLUE);
+//    }
 
 
 //private View.OnClickListener myClickListener = new View.OnClickListener() {
